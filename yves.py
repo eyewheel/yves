cd6bf91f2e4ab381523344deddbde1e54f93b04f
eyewheel <185627573+eyewheel@users.noreply.github.com>
2024-12-08 13:34:46 -0700

    also require author

--- yves.py
+++ yves.py
@@ -2,15 +2,20 @@ import requests
 import json
 import sys
 
-base_url = "https://openlibrary.org/search.json?title="
+base_url = "https://openlibrary.org/search.json?title={}&author={}"
 
 class Book:
-    def __init__(self, canonical_title):
+    def __init__(self, canonical_title, canonical_author):
         self.canonical_title = canonical_title
+        self.canonical_author = canonical_author
         self.raw_reps = []
 
     def add_possible(self, doc):
         self.raw_reps.append(doc)
+
+    def validate_possibles(self):
+        print("about to validate...")
+
 
     def __str__(self):
         return f"{self.canonical_title} - {len(self.raw_reps)} possible versions"
@@ -22,17 +27,21 @@ def import_catalog(file):
     for line in lines:
         line = line.strip()
         if line:
-            import_book(line)
+            title, author = line.split("%%")
+            import_book(title, author)
 
-def import_book(title):
-    book = Book(title)
+def import_book(title, author):
+    book = Book(title, author)
     escaped_title = title.replace(' ', '+')
-    url = base_url + escaped_title
+    escaped_author = author.replace(' ', '+')
+    url = base_url.format(escaped_title, escaped_author)
+    print(url)
     response = requests.get(url)
     data = json.loads(response.text)
     for doc in data['docs']:
         if 'title' in doc:
             book.add_possible(doc)
+    book.validate_possibles()
 
     print(book)
     
